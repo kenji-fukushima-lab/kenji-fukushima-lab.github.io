1f97fa8e93043a14cbee12fd602adc3c7d7641aa
Kenji Fukushima <kfuku52@gmail.com>
2024-01-17 00:12:51 +0100

    languages: en and ja

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -35,11 +35,11 @@ let setTheme = (theme) => {
     for (let i = 0; i < jupyterNotebooks.length; i++) {
       let bodyElement = jupyterNotebooks[i].getElementsByTagName("iframe")[0].contentWindow.document.body;
       if (theme == "dark") {
-        bodyElement.setAttribute("data-jp-theme-light", "false");
-        bodyElement.setAttribute("data-jp-theme-name", "JupyterLab Dark");
+        bodyElement.setAttribute("data-ja-theme-light", "false");
+        bodyElement.setAttribute("data-ja-theme-name", "JupyterLab Dark");
       } else {
-        bodyElement.setAttribute("data-jp-theme-light", "true");
-        bodyElement.setAttribute("data-jp-theme-name", "JupyterLab Light");
+        bodyElement.setAttribute("data-ja-theme-light", "true");
+        bodyElement.setAttribute("data-ja-theme-name", "JupyterLab Light");
       }
     }
   } else {
