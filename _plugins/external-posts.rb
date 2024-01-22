6e84a2b44a5986f62a4a8b9d5fe217d91185a234
Kenji Fukushima <kfuku52@gmail.com>
2024-01-22 21:58:32 +0100

    updates

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -12,21 +12,25 @@ module ExternalPosts
         site.config['external_sources'].each do |src|
           p "Fetching external posts from #{src['name']}:"
           xml = HTTParty.get(src['rss_url']).body
-          feed = Feedjira.parse(xml)
-          feed.entries.each do |e|
-            p "...fetching #{e.url}"
-            slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-            path = site.in_source_dir("_posts/#{slug}.md")
-            doc = Jekyll::Document.new(
-              path, { :site => site, :collection => site.collections['posts'] }
-            )
-            doc.data['external_source'] = src['name'];
-            doc.data['feed_content'] = e.content;
-            doc.data['title'] = "#{e.title}";
-            doc.data['description'] = e.summary;
-            doc.data['date'] = e.published;
-            doc.data['redirect'] = e.url;
-            site.collections['posts'].docs << doc
+          begin
+            feed = Feedjira.parse(xml)
+            feed.entries.each do |e|
+              p "...fetching #{e.url}"
+              slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+              path = site.in_source_dir("_posts/#{slug}.md")
+              doc = Jekyll::Document.new(
+                path, { :site => site, :collection => site.collections['posts'] }
+              )
+              doc.data['external_source'] = src['name'];
+              doc.data['feed_content'] = e.content;
+              doc.data['title'] = "#{e.title}";
+              doc.data['description'] = e.summary;
+              doc.data['date'] = e.published;
+              doc.data['redirect'] = e.url;
+              site.collections['posts'].docs << doc
+            end
+          rescue Feedjira::NoParserAvailable => e
+            puts "Failed to parse the feed: #{e.message}"
           end
         end
       end
